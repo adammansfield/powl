34d7e3ec6478d9996ab3e285aca529b12c06f1fb
Adam Mansfield <adam@adammansfield.org>
2012-03-20 16:05:27 -0400

    Added parsing for transaction processing.

--- powl.py
+++ powl.py
@@ -2,35 +2,52 @@ import imaplib
 import email
 import re
 import sys
+from optparse import OptionParser
 
-# Copy these into a file name config.py to use
-email_username = "default@gmail.com"
-email_password = "1234"
-email_mailbox = "INBOX"
+## Copy these into a file name config.py to use
+#email_username = "default@gmail.com"
+#email_password = "1234"
+#email_mailbox = "INBOX"
 
-try:
-    from config import *
-except ImportError:
-    sys.exit()
+#try:
+#    from config import *
+#except ImportError:
+#    sys.exit()
     
-imap = imaplib.IMAP4_SSL("imap.gmail.com")
-imap.login(email_username, email_password)
-imap.select(email_mailbox)
-search_response, email_id_list = imap.search(None, "(Unseen)")
+#imap = imaplib.IMAP4_SSL("imap.gmail.com")
+#imap.login(email_username, email_password)
+#imap.select(email_mailbox)
+#search_response, email_id_list = imap.search(None, "(Unseen)")
 
-for email_id in email_id_list[0].split():
-    response, data = imap.fetch(email_id, "(RFC822)")
-    mail = email.message_from_string(data[0][1])
+#for email_id in email_id_list[0].split():
+#    response, data = imap.fetch(email_id, "(RFC822)")
+#    mail = email.message_from_string(data[0][1])
     
-    for part in mail.walk():
-        if part.get_content_type() == 'text/html':
-            body = part.get_payload()
+#    for part in mail.walk():
+#        if part.get_content_type() == 'text/html':
+#            body = part.get_payload()
     
-    # parses a koodo to email sms message
-    if (body):
-        result = re.split("<center><p><br><p>", body)
-        result = re.split("<p>\r\n</center>", result[1])
-        print result[0]
+#    # parses a koodo to email sms message
+#    if (body):
+#        result = re.split("<center><p><br><p>", body)
+#        result = re.split("<p>\r\n</center>", result[1])
+#        print result[0]
 
-imap.close()
-imap.logout()
+#imap.close()
+#imap.logout()
+
+def transaction(debit, credit, amount, memo):
+    print(debit)
+
+def main():
+    parser = OptionParser()
+    parser.add_option("--transaction")
+    parser.add_option("-d", type="string", dest="debit", default="")
+    parser.add_option("-c", type="string", dest="credit", default="")
+    parser.add_option("-a", type="string", dest="amount", default="")
+    parser.add_option("-m", type="string", dest="memo", default="")
+    (options, args) = parser.parse_args()
+    transaction(options.debit, options.credit, options.amount, options.memo)
+    
+if __name__ == '__main__':
+    main()
