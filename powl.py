e6bdcc1d75a3fa9038bea8d90d4ce40b1a662a31
Adam Mansfield <adam@adammansfield.org>
2012-04-03 12:33:17 -0400

    Implemented dictionary for functions with QIF files.

--- powl.py
+++ powl.py
@@ -6,41 +6,17 @@ import shutil
 import sys
 from optparse import OptionParser
 
-## Copy these into a file name config.py to use
-#emailself.username = "default@gmail.com"
-#emailself.password = "1234"
-#emailself.mailbox = "INBOX"
-
-#try:
-#    from config import *
-#except ImportError:
-#    sys.exit()
-    
-#imap = imaplib.IMAP4self.SSL("imap.gmail.com")
-#imap.login(emailself.username, email_password)
-#imap.select(emailself.mailbox)
-#searchself.response, email_id_list = imap.search(None, "(Unseen)")
-
-#for emailself.id in email_id_list[0].split():
-#    response, data = imap.fetch(emailself.id, "(RFC822)")
-#    mail = email.messageself.from_string(data[0][1])
-    
-#    for part in mail.walk():
-#        if part.getself.content_type() == 'text/html':
-#            body = part.getself.payload()
-    
-#    # parses a koodo to email sms message
-#    if (body):
-#        result = re.split("<center><p><br><p>", body)
-#        result = re.split("<p>\r\n</center>", result[1])
-#        print result[0]
-
-#imap.close()
-#imap.logout()
-
 class TransactionProcessor:
     """A transaction processor for QIF files."""
     # TODO: replace qif accounts configurable file
+    transaction_files = {
+        "c": "cash.qif",
+        "n": "chequing.qif",
+        "m": "mastercard.qif",
+        "p": "payable.qif",
+        "r": "receivable.qif",
+        "v": "visa.qif",
+    }
     assets = {
         "c": "Assets:Current:Cash",
         "n": "Assets:Current:Chequing",
@@ -78,16 +54,8 @@ class TransactionProcessor:
         "sup":  "Expenses:Upkeep:Supplies",
         "pho":  "Expenses:Utilities:Phone",
     }
-    # TODO: replace hardcoded files with configuration file
-    path_working = os.getcwd()
-    path_default = '{0}/default/'.format(path_working)
-    path_transactions = '{0}/transactions/'.format(path_working)
-    file_cash = 'cash.qif'
-    file_chequing = 'chequing.qif'
-    file_mastercard = 'mastercard.qif'
-    file_payable = 'payable.qif'
-    file_receivable = 'receivable.qif'
-    file_visa = 'visa.qif'
+    path_default = '{0}/default/'.format(os.getcwd())
+    path_transactions = '{0}/transactions/'.format(os.getcwd())
 
 
     def isMultipleMainAccounts(self):
@@ -105,24 +73,14 @@ class TransactionProcessor:
 
 
     def checkForExistingFiles(self):
-        cash_exists = os.path.exists(self.path_transactions + self.file_cash)
-        chequing_exists = os.path.exists(self.path_transactions + self.file_chequing)
-        mastercard_exists = os.path.exists(self.path_transactions + self.file_mastercard)
-        payable_exists = os.path.exists(self.path_transactions + self.file_payable)
-        receivable_exists = os.path.exists(self.path_transactions + self.file_receivable)
-        visa_exists = os.path.exists(self.path_transactions + self.file_visa)
-        if not cash_exists:
-            shutil.copyfile(self.path_default + self.file_cash, self.path_transactions + self.file_cash)
-        if not chequing_exists:
-            shutil.copyfile(self.path_default + self.file_chequing, self.path_transactions + self.file_chequing)
-        if not mastercard_exists:
-            shutil.copyfile(self.path_default + self.file_mastercard, self.path_transactions + self.file_mastercard)
-        if not payable_exists:
-            shutil.copyfile(self.path_default + self.file_payable, self.path_transactions + self.file_payable)
-        if not receivable_exists:
-            shutil.copyfile(self.path_default + self.file_receivable, self.path_transactions + self.file_visa)
-        if not visa_exists:
-            shutil.copyfile(self.path_default + self.file_visa, self.path_transactions + self.file_visa)
+        """Copies default QIF files if missing."""
+        files = self.transaction_files.itervalues()
+        for file in files:
+            custom_file = '{0}/{1}'.format(self.path_transactions, file)
+            exists = os.path.exists(custom_file)
+            if not exists:
+                default_file = '{0}/{1}'.format(self.path_default, file)
+                shutil.copyfile(default_file, custom_file)
 
 
     def Process(self):
