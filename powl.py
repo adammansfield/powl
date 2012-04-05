0718e4cc4efb89c7404903354488c6ee5968db45
Adam Mansfield <adam@adammansfield.org>
2012-04-05 14:46:25 -0400

    Refractored for better encapsulation.

--- powl.py
+++ powl.py
@@ -5,55 +5,47 @@ import re
 import shutil
 import sys
 import time
-import transaction
 import optparse
+from transaction import TransactionProcessor
 
-class Paths:
-    default = os.getcwd() + os.path.sep + 'default'
-    logs = os.getcwd() + os.path.sep + 'logs'
-    transactions = os.getcwd() + os.path.sep + 'transactions'
+class Powl:
+    path_default = os.getcwd() + os.path.sep + 'default'
+    path_logs = os.getcwd() + os.path.sep + 'logs'
+    path_transactions = os.getcwd() + os.path.sep + 'transactions'
 
-def parse_transaction(options):
-    for option in options:
-        if re.match('d', option):
-            debit = option.replace('d ','')
-        elif re.match('c', option):
-            credit = option.replace('c ','')
-        elif re.match('a', option):
-            amount = option.replace('a ','')
-        elif re.match('m', option):
-            memo = option.replace('m ','')
-            memo = memo.replace("\"", '')
+    def parse_message(self, message):
+        """Parses a message and does a corresponding action."""
+        # TODO: parse date from email
+        date = "2012-03-26 10:23:12"
+        date = time.strptime(date, "%Y-%m-%d %H:%M:%S")
 
-def parse_message(message):
-    """Parses a message and does a corresponding action."""
-    options = re.split(' -', message)
-    if options[0] == 'transaction':
-        parse_transaction(options)
-        
-def main():
-    # TODO: replace OptionParser with string parsing
-    # parser = optparse.OptionParser()
-    # parser.add_option("--transaction")
-    # parser.add_option("-d", type="string", dest="debit", default="")
-    # parser.add_option("-c", type="string", dest="credit", default="")
-    # parser.add_option("-a", type="string", dest="amount", default="")
-    # parser.add_option("-m", type="string", dest="memo", default="")
-    # (options, args) = parser.parse_args()
+        options = re.split(' -', message)
+        if options[0] == 'transaction':
+            self.parse_transaction(options, date)
 
-    message = "transaction -d din -c m -a 5.65 -m \"lunch at subway\""
-    parse_message(message)
-
-
-    # TODO: parse date from email
-    # date = "2012-03-26 10:23:12"
-    # date = time.strptime(date, "%Y-%m-%d %H:%M:%S")
-    # accounting = transaction.Transaction(date# ,
-                                         # options.debit,
-                                         # options.credit,
-                                         # options.amount,
-                                         # options.memo)
-    # accounting.Process()
+    def parse_transaction(self, options, date):
+        for option in options:
+            if re.match('d', option):
+                debit = option.replace('d ','')
+            elif re.match('c', option):
+                credit = option.replace('c ','')
+            elif re.match('a', option):
+                amount = option.replace('a ','')
+            elif re.match('m', option):
+                memo = option.replace('m ','')
+                memo = memo.replace("\"", '')
+        self.transaction.Process(date,
+                                 debit,
+                                 credit,
+                                 amount,
+                                 memo)
+            
+    def main(self):
+        self.transaction = TransactionProcessor(self.path_default,
+                                                self.path_transactions,
+                                                self.path_logs)
+        message = "transaction -d din -c m -a 5.65 -m \"lunch at subway\""
+        self.parse_message(message)
     
 if __name__ == '__main__':
-    main()
+    Powl().main()
--- transaction.py
+++ transaction.py
@@ -1,124 +1,125 @@
 import os
 import shutil
 import time
-import powl
 
-class Transaction:
+class TransactionProcessor:
     """A transaction processor for QIF files."""
     # TODO: replace qif accounts and files with a configurable file
     filenames = {
-        "c": "cash.qif",
-        "n": "chequing.qif",
-        "m": "mastercard.qif",
-        "p": "payable.qif",
-        "r": "receivable.qif",
-        "v": "visa.qif",
+        'c': "cash.qif",
+        'n': "chequing.qif",
+        'm': "mastercard.qif",
+        'p': "payable.qif",
+        'r': "receivable.qif",
+        'v': "visa.qif",
     }
     assets = {
-        "c": "Assets:Current:Cash",
-        "n": "Assets:Current:Chequing",
-        "r": "Assets:Current:Receivable",
-        "s": "Assets:Current:Savings",
+        'c': "Assets:Current:Cash",
+        'n': "Assets:Current:Chequing",
+        'r': "Assets:Current:Receivable",
+        's': "Assets:Current:Savings",
     }
     liabilities = {
-        "m": "Liabilities:Mastercard",
-        "p": "Liabilities:Payable",
-        "v": "Liabilities:Visa",
+        'm': "Liabilities:Mastercard",
+        'p': "Liabilities:Payable",
+        'v': "Liabilities:Visa",
     }
     revenues = { 
-        "ear": "Revenue:Earnings",
-        "gif": "Revenue:Gifts",
-        "int": "Revenue:Interest",
-        "mis": "Revenue:Miscellaneous",
-        "por": "Revenue:Portfolio",
+        'ear': "Revenue:Earnings",
+        'gif': "Revenue:Gifts",
+        'int': "Revenue:Interest",
+        'mis': "Revenue:Miscellaneous",
+        'por': "Revenue:Portfolio",
     }
     expenses = {
-        "gas": "Expenses:Auto:Gas",
-        "ins": "Expenses:Auto:Insurance",
-        "mai": "Expenses:Auto:Maintenance & Fees",
-        "clo": "Expenses:Commodities:Clothing",
-        "com": "Expenses:Commodities:Computer",
-        "woe": "Expenses:Commodities:Workout Equipment",
-        "din": "Expenses:Entertainment:Dining",
-        "gam": "Expenses:Entertainment:Games",
-        "ent": "Expenses:Entertainment:General",
-        "out": "Expenses:Entertainment:Outtings",
-        "mis": "Expenses:Miscellanous:General",
-        "gif": "Expenses:Miscellanous:Gifts",
-        "los": "Expenses:Miscellanous:Loss",
-        "eye": "Expenses:Upkeep:Eyewear",
-        "nut": "Expenses:Upkeep:Nutrition",
-        "sup": "Expenses:Upkeep:Supplies",
-        "pho": "Expenses:Utilities:Phone",
+        'gas': "Expenses:Auto:Gas",
+        'ins': "Expenses:Auto:Insurance",
+        'mai': "Expenses:Auto:Maintenance & Fees",
+        'clo': "Expenses:Commodities:Clothing",
+        'com': "Expenses:Commodities:Computer",
+        'woe': "Expenses:Commodities:Workout Equipment",
+        'din': "Expenses:Entertainment:Dining",
+        'gam': "Expenses:Entertainment:Games",
+        'ent': "Expenses:Entertainment:General",
+        'out': "Expenses:Entertainment:Outtings",
+        'mis': "Expenses:Miscellanous:General",
+        'gif': "Expenses:Miscellanous:Gifts",
+        'los': "Expenses:Miscellanous:Loss",
+        'eye': "Expenses:Upkeep:Eyewear",
+        'nut': "Expenses:Upkeep:Nutrition",
+        'sup': "Expenses:Upkeep:Supplies",
+        'pho': "Expenses:Utilities:Phone",
     }
 
-    def checkForExistingFiles(self):
+    def check_for_existing_files(self):
         """Copies default QIF files if custom files are missing."""
         for filename in self.filenames.itervalues():
-            custom_file = powl.Paths.transactions + os.path.sep + filename
+            custom_file = self.transaction_path + os.path.sep + filename
             if not os.path.isfile(custom_file):
-                default_file = powl.Paths.default + os.path.sep + filename
-                shutil.copyfile(default_file, custom_file)
+                default_file = self.default_path + os.path.sep + filename
+                if os.path.isfile(default_file):
+                    shutil.copyfile(default_file, custom_file)
 
-    def logTransactionError(self):
+    def log_error(self, date, debit, credit, amount, memo):
         """Logs the transaction error."""
         logtime = time.strftime('[%H:%M]', time.localtime())
         logdate = time.strftime('%Y-%m-%d', time.localtime())  
-        transactiondate = time.strftime("%m/%d/%Y", self.date)
+        transactiondate = time.strftime('%m/%d/%Y', date)
         message = (logtime +
                    " Date: " + transactiondate +
-                   " Debit: " + self.debit +
-                   " Credit: " + self.credit +
-                   " Amount: " + self.amount +
-                   " Memo: " + self.memo + os.linesep)
+                   " Debit: " + debit +
+                   " Credit: " + credit +
+                   " Amount: " + amount +
+                   " Memo: " + memo + os.linesep)
         logfile = powl.Paths.logs + os.path.sep + logdate + '.log'
         log = open(logfile, 'a')
         log.write(message)
         log.close()
 
-    def formatToQif(self):
+    def format_to_qif(self, date, debit, credit, amount, memo):
         """Formats transaction data into the QIF format for a specific file."""
         accounts = dict(self.assets.items() +
                         self.liabilities.items() +
                         self.revenues.items() +
                         self.expenses.items())
-        if self.debit in self.filenames:
-            filename = self.filenames.get(self.debit)
-            transfer = accounts.get(self.credit)
+        if debit in self.filenames:
+            filename = self.filenames.get(debit)
+            transfer = accounts.get(credit)
         else:
-            filename = self.filenames.get(self.credit)
-            transfer = accounts.get(self.debit)
-        if self.debit in self.expenses:
-            amount = self.amount * -1
-        else:
-            amount = self.amount
-        date = time.strftime("%m/%d/%Y", self.date)
+            filename = self.filenames.get(credit)
+            transfer = accounts.get(debit)
+        if debit in self.expenses:
+            amount = amount * -1
+        date = time.strftime('%m/%d/%Y', date)
         transaction = ("D{0}{1}".format(date, os.linesep) + 
                        "T{0}{1}".format(amount, os.linesep) +
                        "L{0}{1}".format(transfer, os.linesep) +
-                       "M{0}{1}".format(self.memo, os.linesep) +
+                       "M{0}{1}".format(memo, os.linesep) +
                        "^{0}".format(os.linesep))
         return filename, transaction
 
-    def Process(self):
-        """Processes a transaction into the QIF format."""
-        valid_transaction = self.debit in self.filenames or self.credit in self.filenames
-        if valid_transaction:
-            self.checkForExistingFiles()
-            filename, transaction = self.formatToQif()
-            filename = powl.Paths.transactions + os.path.sep + filename 
-            file = open(filename, 'a')
-            file.write(transaction)
-            file.close()
-            print filename
-            print transaction
-        else:
-            self.logTransactionError()
+    def append_transaction_to_file(self, filename, transaction):
+        filename = self.transaction_path + os.path.sep + filename 
+        file = open(filename, 'a')
+        file.write(transaction)
+        file.close()
 
-    def __init__(self, date, debit, credit, amount, memo):
-        """Sets the transaction data."""
-        self.date = date
-        self.debit = debit
-        self.credit = credit
-        self.amount = amount
-        self.memo = memo
+    def Process(self, date, debit, credit, amount, memo):
+        """Processes a transaction into the QIF format."""
+        valid_transaction = debit in self.filenames or credit in self.filenames
+        if valid_transaction:
+            filename, transaction = self.format_to_qif(date,
+                                                       debit,
+                                                       credit,
+                                                       amount,
+                                                       memo)
+            self.check_for_existing_files()
+            self.append_transaction_to_file(filename, transaction)
+        else:
+            self.log_error()
+
+    def __init__(self, default_path, transaction_path, log_path):
+        """Sets the paths for the transaction files."""
+        self.default_path = default_path
+        self.log_path = log_path
+        self.transaction_path = transaction_path
