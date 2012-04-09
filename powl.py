765618fc88e1685e44e48de3c078a60f89298aac
Adam Mansfield <adam@adammansfield.org>
2012-04-09 13:31:19 -0400

    Added email processing for date and message stripped of markup.

--- powl.py
+++ powl.py
@@ -8,58 +8,95 @@ import shutil
 import sys
 import time
 import optparse
-from transaction import TransactionProcessor
+import TransactionProcessor
 logging.basicConfig(level=logging.DEBUG,
                     format='[%(asctime)s %(levelname)s] %(message)s',
                     datefmt='%H:%M')
 
 class Powl:
-    path_default = os.getcwd() + os.sep + 'default'
-    path_logs = os.getcwd() + os.sep + 'logs'
-    path_transactions = os.getcwd() + os.sep + 'transactions'
+    """Class for processing emails to do a corresponding action."""
 
-    def read_inbox(self):
-        config = ConfigParser.ConfigParser()
-        config.readfp(open('config.cfg'))
-        address = config.get('Email','address')
-        password = config.get('Email','password')
-        logging.debug('%s %s', address, password)
-        pass
-        
-    def parse_message(self, message):
+    # Email Processing
+    # ----------------
+    def process_inbox(self):
+        """Processes a list of messages to pass onto parsing."""
+        search_response, email_ids = self.imap.search(None, "(Unseen)")
+        for email_id in email_ids[0].split():
+            fetch_response, data = self.imap.fetch(email_id, "(RFC822)")
+            mail = email.message_from_string(data[0][1])
+            date = email.utils.parsedate(mail['Date'])
+            for part in mail.walk():
+                if part.get_content_type() == 'text/html':
+                    body = part.get_payload()
+                    message = self.strip_message_markup(body)
+                    logging.debug('EMAIL: %s - %s', message, date)
+                    self.parse_message(message, date)
+
+    def strip_message_markup(self, message):
+        """Returns email message striped of markup."""
+        retval = message
+        retval = retval.replace('<P>','')
+        retval = retval.replace('</P>','')
+        retval = retval.replace('=0A',' ')
+        return retval
+
+    # Parsing
+    # -------
+    def parse_message(self, message, date):
         """Parses a message and does a corresponding action."""
         # TODO: parse date from email
-        date = "2012-03-26 10:23:12"
-        date = time.strptime(date, "%Y-%m-%d %H:%M:%S")
+        params = re.split(' -', message)
+        if params[0].strip() == 'transaction':
+            self.parse_transaction(params, date)
+        else:
+            # TODO: append to miscellaneous file
+            logging.debug('No match: %s', message)
 
-        options = re.split(' -', message)
-        if options[0] == 'transaction':
-            self.parse_transaction(options, date)
-
-    def parse_transaction(self, options, date):
-        for option in options:
-            if re.match('d', option):
-                debit = option.replace('d ','')
-            elif re.match('c', option):
-                credit = option.replace('c ','')
-            elif re.match('a', option):
-                amount = option.replace('a ','')
-            elif re.match('m', option):
-                memo = option.replace('m ','')
+    def parse_transaction(self, params, date):
+        """Separates transaction data and processes the transaction."""
+        for param in params:
+            if re.match('d', param):
+                debit = param.replace('d ','')
+            elif re.match('c', param):
+                credit = param.replace('c ','')
+            elif re.match('a', param):
+                amount = param.replace('a ','')
+            elif re.match('m', param):
+                memo = param.replace('m ','')
                 memo = memo.replace("\"", '')
-        self.transaction.Process(date,
-                                 debit,
-                                 credit,
-                                 amount,
-                                 memo)
-            
+        log = 'Debit: %s Credit: %s Amount: %s Memo: %s'
+        logging.debug(log, debit, credit, amount, memo)
+        self.transaction.Process(date, debit, credit, amount, memo)
+    
+
+    # Initialization
+    # --------------
     def main(self):
-        self.read_inbox()
-        #self.transaction = TransactionProcessor(self.path_default,
-        #                                        self.path_transactions,
-        #                                        self.path_logs)
-        #message = "transaction -d din -c m -a 5.65 -m \"lunch at subway\""
-        #self.parse_message(message)
+        self.load_config()
+        self.initialize_modules()
+        self.process_inbox()
+
+    def load_config(self):
+        """Reads custom config file and loads settings."""
+        config = ConfigParser.ConfigParser()
+        config.readfp(open('config.cfg'))
+        self.address = config.get('Email','address')
+        self.password = config.get('Email','password')
+        self.mailbox = config.get('Email', 'mailbox')
+        workingdir = os.getcwd() + os.sep
+        self.path_default = workingdir + config.get('Paths', 'default')
+        self.path_logs = workingdir + config.get('Paths', 'logs')
+        self.path_transactions = workingdir + config.get('Paths', 'transactions')
+        logging.debug('%s %s %s', self.address, self.password, self.mailbox)
+
+    def initialize_modules(self):
+        """Intializes modules used for doing various actions."""
+        self.imap = imaplib.IMAP4_SSL("imap.gmail.com")
+        self.imap.login(self.address, self.password)
+        self.imap.select(self.mailbox)
+        self.transaction = TransactionProcessor.TransactionProcessor(self.path_default,
+                                                                     self.path_transactions,
+                                                                     self.path_logs)
     
 if __name__ == '__main__':
     Powl().main()
