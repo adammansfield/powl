8048c3496a616e2b7d4e3e233f5b49f11f46d6c2
Adam Mansfield <adam@adammansfield.org>
2012-05-07 15:47:34 -0400

    Cleaned up logger default settings.

--- powl/logger.py
+++ powl/logger.py
@@ -5,7 +5,16 @@ import time
 class Logger:
     """Class for logging messages to stream and to file."""
 
+    # LOG FILE
     log_dir = 'logs'
+    filename_base = time.strftime('%Y-W%W', time.localtime())
+    filename_extension = 'log'
+    filename = filename_base + '.' + filename_extension
+
+    # FORMAT
+    log_format = '%(asctime)s\t%(levelname)s\t%(message)s'
+    log_dateformat = '%m-%d %H:%M'
+    formatter = logging.Formatter(log_format, log_dateformat)
 
     # LOGGING
     def critical(self, message, *args, **kwargs):
@@ -30,10 +39,9 @@ class Logger:
 
     # HANDLERS
     def set_file_handler(self, filedir):
-        """Set file to which to direct log messages."""
-        filename = time.strftime('%Y-%m-%d', time.localtime()) + '.log'
-        file = filedir + os.sep + filename
-        handler = logging.FileHandler(file)
+        """Set file, level, and format for the log to file messages."""
+        filepath = os.path.join(filedir, self.filename)
+        handler = logging.FileHandler(filepath)
         handler.setLevel(self.level)
         handler.setFormatter(self.formatter)
         self.logger.addHandler(handler)
@@ -46,22 +54,15 @@ class Logger:
         self.logger.addHandler(streamhandler)
 
     # INITIALIZATION
-    def __init__(self, loggername="", filedir="", level=logging.DEBUG):
-        """Initialize the file and stream handlers."""
-        # TODO: clean up this
-        self.logger = logging.getLogger(loggername)
-        self.logger.setLevel(level)
-        logformat = '%(asctime)s\t%(levelname)s\t%(message)s'
-        dateformat = '%H:%M'
-        self.formatter = logging.Formatter(logformat, dateformat)
+    def __init__(self, name='', output_dir='', level=logging.DEBUG):
+        """Initialize the stream and file handlers."""
+        self.name = name
         self.level = level
+        self.logger = logging.getLogger(self.name)
+        self.logger.setLevel(self.level)
         self.set_stream_handler()
-        if filedir != "":
-            filedir = filedir + os.sep + self.log_dir
-            self.create_folder_if_missing(filedir)
-            self.set_file_handler(filedir)
-
-    def create_folder_if_missing(self, filedir):
-        """Create logs folder if missing."""
-        if not os.path.isdir(filedir):
-            os.makedirs(filedir)
+        if output_dir:
+            log_dir = os.path.join(output_dir, self.log_dir)
+            if not os.path.isdir(log_dir):
+                os.makedirs(log_dir)
+            self.set_file_handler(log_dir)
