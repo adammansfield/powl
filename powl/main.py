2e009ec8639a5bf4b31c03113f2fe07f609645ea
Adam Mansfield <adam@adammansfield.org>
2012-05-28 14:03:57 -0400

    Added main for running powl.

--- powl/main.py
+++ powl/main.py
@@ -3,11 +3,10 @@
 import sys
 from powl.powl import Powl
 
-
 def main(*args):
+    """Create an instance of powl and process a mailbox."""
     powl = Powl()
-    powl.process_mailbox()
-
+    powl.process()
 
 if __name__ == '__main__':
     main(*sys.argv[1:])
