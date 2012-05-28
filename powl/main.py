4ff96dcb04429ff726eedcb75c93e2736d8a9304
Adam Mansfield <adam@adammansfield.org>
2012-05-28 14:57:28 -0400

    Updated to match new class name.

--- powl/main.py
+++ powl/main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 """Main script for running powl."""
 import sys
-from powl.powl import Powl
+from powl.controller import Controller
 
 def main(*args):
     """Create an instance of powl and process a mailbox."""
-    powl = Powl()
-    powl.process()
+    controller = Controller()
+    controller.process()
 
 if __name__ == '__main__':
     main(*sys.argv[1:])
