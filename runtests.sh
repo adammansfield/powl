73e0f52987666f3bb2d6a291616e049fd03898a7
Adam Mansfield <adam@adammansfield.org>
2012-05-28 14:56:18 -0400

    Renamed to match new test folder name.

--- runtests.sh
+++ runtests.sh
@@ -3,7 +3,7 @@ export PYTHONPATH=.
 echo
 echo "Transaction Processor Tests"
 echo "==========================="
-python powl/tests/test_processor_transaction.py
+python powl/test/test_processor_transaction.py
 #echo
 #echo "Parse Tests"
 #echo "==========="
