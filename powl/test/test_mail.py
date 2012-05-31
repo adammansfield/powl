221e5c8cb74821fbfd17775932990c606f610601
Adam Mansfield <adam@adammansfield.org>
2012-05-31 16:57:58 -0400

    Added tests for mailbox selection for failure, success, and config.

--- powl/test/test_mail.py
+++ powl/test/test_mail.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import socket
 import unittest
+import powl.logger as logger
 from powl.config import Config
 from powl.mail import Mail
 
@@ -9,11 +10,17 @@ class MailTest(unittest.TestCase):
     empty_server = ''
     empty_address = ''
     empty_password = ''
-    fake_server = 'foo'
+    empty_mailbox = ''
+    fake_server = 'abiCi8d2fo'
     fake_address = 'Btu72Bu7atu'
     fake_password = '8u89HuhiOUU'
+    fake_mailbox = 'k8d3bka9i'
     gmail_server = 'imap.gmail.com'
+    gmail_mailbox = 'inbox'
     nonimap_server = 'google.ca'
+
+    # CONFIG ON DATA
+    config_isread = False
 
     # IMAP SETUP
     def test_imap_empty(self):
@@ -78,20 +85,73 @@ class MailTest(unittest.TestCase):
 
     def test_login_config(self):
         """Test login with config creditials and should succeed if correct."""
-        try:
-            config = Config()
-            config.read()
-        except Exception as e:
-            logger.error("Config.cfg was not found for intergation test.")
-            self.fail(e)
-        mail = Mail(config.server,
-                    config.address,
-                    config.password)
+        server, address, password, mailbox = self._get_config_creditials()
+        mail = Mail(server, address, password)
         try:
             mail._get_imap()
             mail._login()
         except Exception as e:
             self.fail("Unexpected exception." + str(e))
+
+    # MAILBOX SELECTION
+    def test_mailbox_failure(self):
+        """Test mailbox selection with a fake mailbox."""
+        server, address, password, mailbox = self._get_config_creditials()
+        mailbox = self.fake_mailbox
+        mail = Mail(server, address, password, mailbox)
+        try:
+            mail._get_imap()
+            mail._login()
+        except Exception as e:
+            self.fail("Setup failed before test with unexpected exception." + str(e))
+        expected = Mail.MailboxSelectionError
+        self.assertRaises(expected, mail._select_mailbox)
+
+    def test_mailbox_success(self):
+        """Test mailbox selection success with default inbox."""
+        server, address, password, mailbox = self._get_config_creditials()
+        mailbox = 'inbox'
+        mail = Mail(server, address, password, mailbox)
+        try:
+            mail._get_imap()
+            mail._login()
+        except Exception as e:
+            self.fail("Setup failed before test with unexpected exception." + str(e))
+        try:    
+            mail._select_mailbox()
+        except Exception as e:
+            self.fail("Unexpected exception. " + str(e))
+
+    def test_mailbox_config(self):
+        """Test mailbox selection success with config mailbox."""
+        server, address, password, mailbox = self._get_config_creditials()
+        mail = Mail(server, address, password, mailbox)
+        try:
+            mail._get_imap()
+            mail._login()
+        except Exception as e:
+            self.fail("Setup failed before test with unexpected exception." + str(e))
+        try:    
+            mail._select_mailbox()
+        except Exception as e:
+            self.fail("Unexpected exception. " + str(e))
+
+    # HELPER FUNCTIONS
+    def _get_config_creditials(self):
+        """Get server, address, and password creditials from config file."""
+        if not self.config_isread:
+            try:
+                config = Config()
+                config.read()
+                self.config_isread = True
+                self.config_server = config.server
+                self.config_address = config.address
+                self.config_password = config.password
+                self.config_mailbox = config.mailbox
+            except Exception as e:
+                logger.error("Config.cfg was not found for intergation test.")
+                self.fail(e)
+        return self.config_server, self.config_address, self.config_password, self.config_mailbox
 
 if __name__ == '__main__':
     unittest.main()
