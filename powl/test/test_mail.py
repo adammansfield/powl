2ad1cb7b40cf0bd35cd706c9b16630dda72d3e9d
Adam Mansfield <adam@adammansfield.org>
2012-05-31 14:43:31 -0400

    Added test for successfully getting imap object.

--- powl/test/test_mail.py
+++ powl/test/test_mail.py
@@ -1,27 +1,37 @@
 #!/usr/bin/env python
 import socket
 import unittest
+from powl.config import Config
 from powl.mail import Mail
 
 class MailTest(unittest.TestCase):
-    server_notexist = 'blah'
-    server_notimap = 'google.ca'
-    
-    def test_imap_notfound(self):
-        server = self.server_notimap
-        address = ''
-        password = ''
-        mail = Mail(server, address, password)
+    server_gmail = 'imap.gmail.com'
+    server_timeout = 'google.ca'
+    server_unknown = 'foo'
+
+    def test_imap_success(self):
+        """Test imap connection success."""
+        mail = Mail(self.server_gmail, '', '')
+        try:
+            mail._get_imap()
+        except:
+            self.fail("Unexpected exception raised.")
+
+    def test_imap_timeout(self):
+        """Test imap timeout."""
+        mail = Mail(self.server_timeout, '', '')
         expected = Mail.ServerTimedOutError
         self.assertRaises(expected, mail._get_imap)
 
     def test_imap_unknown(self):
-        server = self.server_notexist
-        address = ''
-        password = ''
-        mail = Mail(server, address, password)
+        """Test imap with an unknown address."""
+        mail = Mail(self.server_unknown, '', '')
         expected = Mail.ServerUnknownError
         self.assertRaises(expected, mail._get_imap)
+
+    def test_login_fail(self):
+        """Test login with wrong creditionals."""
+        pass
 
 if __name__ == '__main__':
     unittest.main()
