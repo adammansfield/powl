3bebc86aab2f5f40228f265755aae3cbb5647b31
Adam Mansfield <adam@adammansfield.org>
2012-05-31 15:47:25 -0400

    Added mail test to runtests.sh.

--- runtests.sh
+++ runtests.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-export PYTHONPATH=.
 echo
-echo "Transaction Processor Tests"
-echo "==========================="
+echo "Processor - Transaction"
+echo "======================="
 python powl/test/test_processor_transaction.py
-#echo
-#echo "Parse Tests"
-#echo "==========="
-#python powl/tests/test_processor_action.py
+echo
+echo "Mail"
+echo "===="
+python powl/test/test_mail.py
