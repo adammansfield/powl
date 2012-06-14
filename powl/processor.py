99a815402d004f1a2bba1f1582be8aaf9b6ee1c6
Adam Mansfield <adam@adammansfield.org>
2012-06-14 11:40:00 -0400

    Added parse message function with action types.

--- powl/processor.py
+++ powl/processor.py
@@ -1,3 +1,28 @@
 #!/usr/bin/env python
 """Parse actions and processing for simpler actions."""
 
+class Processor:
+
+    action_bodycomposition = 'bodycomposition'
+    action_event = 'event'
+    action_note = 'note'
+    action_todo = 'todo'
+    action_transaction = 'transaction'
+    _action_list = [
+        action_bodycomposition,
+        action_event,
+        action_note,
+        action_todo,
+        action_transaction
+    ]
+    action_nomatch = 'nomatch'
+
+    def parse_message(self, message):
+        """Parse a message and determine its specified action."""
+        raw_action, data = message.split(' ', 1)
+        if raw_action in self._action_list:
+            action = raw_action
+        else:
+            action = self.action_nomatch
+            data = message
+        return action, data
