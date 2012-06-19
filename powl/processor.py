7a55aedfb2d933f29b9432f4244da82c5d8dd24b
Adam Mansfield <adam@adammansfield.org>
2012-06-19 15:41:59 -0400

    Moved action types to actions.py.

--- powl/processor.py
+++ powl/processor.py
@@ -1,30 +1,17 @@
 #!/usr/bin/env python
 """Parse actions and processing for simpler actions."""
 import re
+from powl.actions import Actions
 
-class Processor:
-
-    action_bodycomposition = 'bodycomposition'
-    action_event = 'event'
-    action_note = 'note'
-    action_todo = 'todo'
-    action_transaction = 'transaction'
-    _action_list = [
-        action_bodycomposition,
-        action_event,
-        action_note,
-        action_todo,
-        action_transaction
-    ]
-    action_nomatch = 'nomatch'
+class MessageParser:
 
     def parse_message(self, message):
         """Parse a message and determine its specified action."""
         raw_action, data = message.split(' ', 1)
-        if raw_action in self._action_list:
+        if raw_action in Actions.actionable_list:
             action = raw_action
         else:
-            action = self.action_nomatch
+            action = Actions.nomatch
             data = message
         return action, data
 
