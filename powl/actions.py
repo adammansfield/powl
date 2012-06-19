009e2337bc3a1e1b56075dd0dd8b72cd3b1c17be
Adam Mansfield <adam@adammansfield.org>
2012-06-19 17:10:09 -0400

    Moved parsing to actions. Completed processing for miscellaneous and transactions.

--- powl/actions.py
+++ powl/actions.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 """Perform actions with given data."""
-from powl.transaction import Transaction
+import os
+import re
+import powl.output as output
 
 class Actions:
     """Contains definitions for possible actions."""
@@ -11,16 +13,18 @@ class Actions:
     todo = 'todo'
     transaction = 'transaction'
     actionable_list = [
-        action_bodycomposition,
-        action_event,
-        action_note,
-        action_todo,
-        action_transaction
+        bodycomposition,
+        event,
+        note,
+        todo,
+        transaction
     ]
 
 
-    def do_action(self, action, data, date):
+    # ACTIONS
+    def do_action(self, message, date):
         """Determine and do the specified action."""
+        action, data = self._parse_message(message)
         self._action_to_method_map[action](data, date)
 
     def _process_bodycomposition(self, data, date):
@@ -30,7 +34,10 @@ class Actions:
         pass
 
     def _process_note(self, data, date):
-        pass
+        # TODO: replace next two lines with data.py interface
+        filepath = os.path.join(self._output_dir, 'miscellaneous.txt')
+        data = data + os.linesep
+        output.append(filename, data)
 
     def _process_nomatch(self, data, date):
         pass
@@ -40,18 +47,67 @@ class Actions:
 
     def _process_transaction(self, data, date):
         """Separate transaction data to pass onto processing."""
-        debit, credit, amount, memo = self.message.transaction(params)
-        filename, data = self.transaction.process(date, debit, credit, amount, memo)
-        output.append(filename, data)
+        debit, credit, amount, memo = self._parse_transaction(data)
+        filename, data = self._transaction.process(date, debit, credit, amount, memo)
+        # TODO: replace next three lines with data.py interface
+        if filename:
+            filepath = os.path.join(self._transaction_dir, filename)
+            output.append(filepath, data)
 
-    def __init__(self, config):
-        self._config = config
-        self._transaction = Transaction(self._config.qif_filenames,
-                                        self._config.qif_types,
-                                        self._config.qif_assets,
-                                        self._config.qif_liabilities,
-                                        self._config.qif_revenues,
-                                        self._config.qif_expenses)
+    # PARSING
+    def _parse_message(self, message):
+        """Parse a message and determine its specified action."""
+        raw_action, data = message.split(' ', 1)
+        if raw_action in Actions.actionable_list:
+            action = raw_action
+        else:
+            action = Actions.nomatch
+            data = message
+        return action, data
+
+    def _parse_transaction(self, data):
+        """Parse a transaction data into debit, credit, amount and memo."""
+        debit = ''
+        credit = ''
+        amount = ''
+        memo = ''
+        params = re.split('-', data)
+        for param in params:
+            if re.match('^d', param):
+                debit = re.sub('^d', '', param)
+            elif re.match('^c', param):
+                credit = re.sub('^c', '', param)
+            elif re.match('^a', param):
+                amount = re.sub('^a', '', param)
+            elif re.match('^m', param):
+                memo = re.sub('^m', '', param)
+                memo = memo.replace("\"", '')
+        debit = debit.strip()
+        credit = credit.strip()
+        amount = amount.strip()
+        memo = memo.strip()
+        return debit, credit, amount, memo
+
+    def _parse_bodycomposition(self, data):
+        """Parse a body composition data into mass and body fat percentage."""
+        mass = ''
+        fat = ''
+        params = re.split('-', data)
+        for param in params:
+            if re.match('^m', param):
+                mass = re.sub('^m', '', param)
+            elif re.match('^f', param):
+                fat = re.sub('^f', '', param)
+        mass = mass.strip()
+        fat = fat.strip()
+        return mass, fat
+
+    # INITIALIZATION
+    def __init__(self, mail, transaction_processor, output_dir, transaction_dir):
+        self._mail = mail
+        self._transaction = transaction_processor
+        self._output_dir = output_dir   # TODO: abstract this to data.py
+        self._transaction_dir = transaction_dir # TODO: abstract this to data.py
         self._action_to_method_map = {
             self.nomatch: self._process_nomatch,
             self.bodycomposition: self._process_bodycomposition,
