3e7648feb317c0209cce669e578e2a92daadcfc1
Adam Mansfield <adam@adammansfield.org>
2012-06-29 15:23:38 -0400

    Separated out parsing.

---
+++ powl/parser.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""Perform actions with given data."""
+import re
+from powl.actiontypes import ActionTypes
+
+class Parser:
+
+    def parse_message(self, message):
+        """Parse a message and determine its specified action."""
+        raw_action, data = message.split(' ', 1)
+        if raw_action in ActionTypes.actionable:
+            action = raw_action
+        else:
+            action = ActionTypes.nomatch
+            data = message
+        return action, data
+
+    def parse_transaction(self, data):
+        """Parse a transaction data into debit, credit, amount and memo."""
+        debit = ''
+        credit = ''
+        amount = ''
+        memo = ''
+        params = re.split('-', data)
+        for param in params:
+            if re.match('^d', param):
+                debit = re.sub('^d', '', param)
+            elif re.match('^c', param):
+                credit = re.sub('^c', '', param)
+            elif re.match('^a', param):
+                amount = re.sub('^a', '', param)
+            elif re.match('^m', param):
+                memo = re.sub('^m', '', param)
+                memo = memo.replace("\"", '')
+        debit = debit.strip()
+        credit = credit.strip()
+        amount = amount.strip()
+        memo = memo.strip()
+        return debit, credit, amount, memo
+
+    def parse_bodycomposition(self, data):
+        """Parse a body composition data into mass and body fat percentage."""
+        mass = ''
+        fat = ''
+        params = re.split('-', data)
+        for param in params:
+            if re.match('^m', param):
+                mass = re.sub('^m', '', param)
+            elif re.match('^f', param):
+                fat = re.sub('^f', '', param)
+        mass = mass.strip()
+        fat = fat.strip()
+        return mass, fat
