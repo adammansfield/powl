1a51d3f3ab4c8f3b549f0935ca58d0a7642854e1
Adam Mansfield <adam@adammansfield.org>
2013-02-19 05:22:03 -0500

    Created action to process a note.

---
+++ powl/actions/note.py
@@ -0,0 +1,11 @@
+"""Action to process a note."""
+from powl.actions.fileaction import FileAction
+
+class FileAction(FileAction):
+
+    # PROCESSING
+    def process(self):
+        """Process the data into the proper output format."""
+        self._output_data = 
+        if not self._output_filepath:
+            output.append(self._output_filepath, self._output_data)
