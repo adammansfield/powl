5b855a3816a001140a03c4db786c709f78afa4fc
Adam Mansfield <adam@adammansfield.org>
2013-02-19 20:58:54 -0500

    Fixed the output for note action.

--- powl/actions/note.py
+++ powl/actions/note.py
@@ -6,6 +6,4 @@ class FileAction(FileAction):
     # PROCESSING
     def process(self):
         """Process the data into the proper output format."""
-        self._output_data = 
-        if not self._output_filepath:
-            output.append(self._output_filepath, self._output_data)
+        self._output_data = self._input_data
