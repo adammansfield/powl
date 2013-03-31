84489b727a54ddace87c55289e16dfa5bea0607c
Adam Mansfield <adam@adammansfield.org>
2013-03-31 10:16:40 -0400

    Added initialization for output file.

--- powl/actions/note.py
+++ powl/actions/note.py
@@ -3,7 +3,17 @@ from powl.actions.fileaction import FileAction
 
 class Note(FileAction):
 
+    # I/O
+    def input(self, data, date):
+        """Parse the input data."""
+        self._note = data
+
     # PROCESSING
     def process(self):
         """Process the data into the proper output format."""
-        self._output_data = self._input_data
+        self._output_data = self._note
+
+    # INITIALIZATION
+    def __init__(self, filepath):
+        self._output_filepath = filepath
+      
