e94a87810c3bd6551726a8a9ea47c6941efe6340
Adam Mansfield <adam@adammansfield.org>
2013-07-29 23:47:32 -0400

    Refactored LogWriter to set the formatter.

--- powl/logwriter.py
+++ powl/logwriter.py
@@ -1,56 +1,66 @@
-#!/usr/bin/env python
-"""Module for easily directing logging output to streams and files."""
-import errno
+"""Provides methods for logging to file and stream."""
 import logging
-import os
 import time
 
 class LogWriter(object):
+    """
+    Provides methods for writing log messages of various levels to file and stream handlers.
+    """
 
-    def __init__(self, formatter, path, filename, level=logging.INFO):
-        """Get a logger and set stream and file handlers."""
+    _EXTENSION = ".log"
+    _MESSAGE_FORMAT = "%(asctime)s\t%(levelname)s\t%(message)s"
+    _DATE_FORMAT = "%H:%M"
+
+    def __init__(self, folder, level=logging.INFO):
+        """
+        Initialize logger. Create and attach logging handlers. Create log file.
+
+        Args:
+            folder (powl.filesystem.Folder): Folder where the log file will be stored.
+        """
         self._logger = logging.getLogger()
-        self._logger.setLevel(level) 
-        self._set_stream_handler(formatter, level)
-        self._set_file_handler(formatter, level, path, filename)
+
+        formatter = logging.Formatter(self._MESSAGE_FORMAT, self._DATE_FORMAT)
+        filename = time.strftime('%Y-%m-%d', time.localtime()) + self._EXTENSION
+        filestream = folder.get_file(filename)
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(level)
+        stream_handler.setFormatter(formatter)
+        self._logger.addHandler(stream_handler)
+
+        file_handler = logging.FileHandler(filestream.path)
+        file_handler.setLevel(level)
+        file_handler.setFormatter(formatter)
+        self._logger.addHandler(file_handler)
 
     def critical(self, message, *args, **kwargs):
-        """Log a critical message."""
+        """
+        Log a critical message.
+        """
         self._logger.critical(message, *args, **kwargs)
 
     def debug(self, message, *args, **kwargs):
-        """Log a debug message."""
+        """
+        Log a debug message.
+        """
         self._logger.debug(message, *args, **kwargs) 
 
     def error(self, message, *args, **kwargs):
-        """Log a error message."""
+        """
+        Log a error message.
+        """
         self._logger.error(message, *args, **kwargs)
 
     def info(self, message, *args, **kwargs):
-        """Log a info message."""
+        """
+        Log a info message.
+        """
         self._logger.info(message, *args, **kwargs)
 
     def warning(self, message, *args, **kwargs):
-        """Log a warning message."""
+        """
+        Log a warning message.
+        """
         self._logger.warning(message, *args, **kwargs)
-
-    def _set_stream_handler(self, formatter, level):
-        """Set level and format for the trace messages."""
-        handler = logging.StreamHandler()
-        handler.setLevel(level)
-        handler.setFormatter(formatter)
-        self._logger.addHandler(handler)
-
-    def _set_file_handler(self, formatter, level, path, filename):
-        """Set file path, level, and format for log messages."""
-        try:
-            os.makedirs(path)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
-        filepath = os.path.join(path, filename)
-        handler = logging.FileHandler(filepath)
-        handler.setLevel(level)
-        handler.setFormatter(formatter)
-        self._logger.addHandler(handler)
 
