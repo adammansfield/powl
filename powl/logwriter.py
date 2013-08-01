29edabb0cb658da7fea8868ad5b5146dde894149
Adam Mansfield <adam@adammansfield.org>
2013-07-31 22:30:36 -0400

    Added filename date format.

--- powl/logwriter.py
+++ powl/logwriter.py
@@ -1,4 +1,4 @@
-"""Provides methods for logging to file and stream."""
+"""Provides methods for logging to a file and a stream."""
 import logging
 import time
 
@@ -7,25 +7,23 @@ class LogWriter(object):
     Provides methods for writing log messages of various levels to file and stream handlers.
     """
 
-    _EXTENSION = ".log"
     _MESSAGE_FORMAT = "%(asctime)s\t%(levelname)s\t%(message)s"
-    _DATE_FORMAT = "%H:%M"
+    _MESSAGE_DATE_FORMAT = "%H:%M"
+    _FILENAME_DATE_FORMAT = "%Y-%m-%d"
+    _FILENAME_EXTENSION = ".log"
 
-    def __init__(
-        self,
-        folder,               # powl.filesystem.Folder
-        level=logging.INFO):  # logging.level
+    def __init__(self, folder, level=logging.INFO):
         """
         Initialize logger. Create and attach logging handlers. Create log file.
 
         Args:
-            folder: Folder where the log file will be stored.
-            level: Optional log level of the handlers.
+            folder (powl.filesystem.Folder): Folder where the log file will be stored.
+            level (int): Optional log level of the handlers.
         """
         self._logger = logging.getLogger()
 
-        formatter = logging.Formatter(self._MESSAGE_FORMAT, self._DATE_FORMAT)
-        filename = time.strftime('%Y-%m-%d', time.localtime()) + self._EXTENSION
+        formatter = logging.Formatter(self._MESSAGE_FORMAT, self._MESSAGE_DATE_FORMAT)
+        filename = time.strftime(self._FILENAME_DATE_FORMAT, time.localtime()) + self._FILENAME_EXTENSION
         filestream = folder.get_file(filename)
 
         stream_handler = logging.StreamHandler()
@@ -41,30 +39,54 @@ class LogWriter(object):
     def critical(self, message, *args, **kwargs):
         """
         Log a critical message.
+
+        Args:
+            message (string): Message to log.
+            args (strings, ints, or floats): Arguments to be merged into message.
+            kwargs (dict): Arguments used for user defined attributes.
         """
         self._logger.critical(message, *args, **kwargs)
 
     def debug(self, message, *args, **kwargs):
         """
         Log a debug message.
+
+        Args:
+            message (string): Message to log.
+            args (strings, ints, or floats): Arguments to be merged into message.
+            kwargs (dict): Arguments used for user defined attributes.
         """
         self._logger.debug(message, *args, **kwargs) 
 
     def error(self, message, *args, **kwargs):
         """
         Log an error message.
+
+        Args:
+            message (string): Message to log.
+            args (strings, ints, or floats): Arguments to be merged into message.
+            kwargs (dict): Arguments used for user defined attributes.
         """
         self._logger.error(message, *args, **kwargs)
 
     def info(self, message, *args, **kwargs):
         """
         Log an info message.
+
+        Args:
+            message (string): Message to log.
+            args (strings, ints, or floats): Arguments to be merged into message.
+            kwargs (dict): Arguments used for user defined attributes.
         """
         self._logger.info(message, *args, **kwargs)
 
     def warning(self, message, *args, **kwargs):
         """
         Log a warning message.
+
+        Args:
+            message (string): Message to log.
+            args (strings, ints, or floats): Arguments to be merged into message.
+            kwargs (dict): Arguments used for user defined attributes.
         """
         self._logger.warning(message, *args, **kwargs)
-
