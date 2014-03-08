b5323c38b0afec70623d8c5119505ee5370d43d0
Adam Mansfield <adam@adammansfield.org>
2014-03-08 16:56:44 -0500

    Added base parser.

--- powl/parser.py
+++ powl/parser.py
@@ -3,38 +3,116 @@ import re
 from powl import actiontype
 from powl import actiondata
 
-# TODO: create a Parser class with a parse method. This will
-#       be inherited by all the parsers.
 
-class MessageParser:
+class Parser(object):
+    """
+    Provides methods to parse a string into a desired data structure.
+    """
+
+    def parse(self, string):
+        """
+        Parse a string into a desired data structure.
+
+        Parameters
+        ----------
+        string : str
+            String to parse.
+        """
+        pass
+
+
+class BodyCompositionDataParser(Parser):
+    """
+    Parses a formatted string containing body composition data.
+    """
+
+    _TOKEN_FLAG = '-'
+    _TOKEN_MASS = '^m'
+    _TOKEN_FAT_PERCENTAGE = '^f'
+
+    def parse(self, string):
+        """
+        Parse a formatted string into a BodyCompositionData object.
+
+        Parameters
+        ----------
+        string : str
+            String to parse.
+
+        Returns
+        -------
+        powl.actiondata.BodyCompositionData
+            Contains data to perform a body composition action.
+
+        Raises
+        ------
+        ValueError
+            If mass or fat percentage is not a float.
+            If a value for BodyCompositionData is missing.
+        """
+        data = actiondata.BodyCompositionData()
+        params = re.split(self._TOKEN_FLAG, string)
+
+        for param in params:
+            if re.match(self._TOKEN_MASS, param):
+                data.mass = re.sub(self._TOKEN_MASS, '', param)
+                data.mass = data.mass.strip()
+            elif re.match(self._TOKEN_FAT, param):
+                data.fat_percentage = re.sub(self._TOKEN_FAT, '', param)
+                data.fat_percentage = data.fat_percentage.strip()
+
+        if not data.mass:
+            raise ValueError("Mass was not parsed")
+        if not data.fat:
+            raise ValueError("Fat was not parsed")
+
+        try:
+            float(data.mass)
+        except ValueError:
+            raise ValueError("Mass is not a number")
+        try:
+            float(data.fat_percentage)
+        except ValueError:
+            raise ValueError("Fat percentage is not a number")
+
+        return data
+
+
+class MessageParser(Parser):
     """
     Parses a formatted message containing an action and its data.
     """
 
     _DELIMITER = ' '
 
-    def parse(self, message):
+    def parse(self, string):
         """
         Parse a message into an action and its data string.
-        
-        Args:
-            message (str): Message to parse.
-        
-        Returns (powl.actiontype, string):
-            A tuple of an action type and a string containing data.
-        
-        Raises:
-            ValueError: If a valid action was not parsed.
+
+        Parameters
+        ----------
+        message : str
+            Message to parse.
+
+        Returns
+        -------
+        tuple of powl.actiontype and string
+            The action type and the string containing data for the action.
+
+        Raises
+        ------
+        ValueError
+            If a valid action was not parsed.
         """
         action_type, data = message.split(self._DELIMITER, 1)
-        
+
         if action_type not in actiontype.ACTIONABLE:
             Raise ValueError("Action type was not parsed")
-            
+
         return action_type, data
- 
- 
-class TransactionParser(object):
+
+
+class TransactionDataParser(Parser):
     """
     Parses a formatted string containing accounting data.
     """
@@ -47,17 +125,27 @@ class TransactionParser(object):
 
     def parse(self, string):
         """
-        Parse a formatted accounting string into an AccountingData object.
-        
-        Args:
-            string (str): String to parse.
-        
-        Raises:
-            ValueError: If amount is not a float. If a value for AccountingData is missing.
+        Parse a formatted string into a TransactionData object.
+
+        Parameters
+        ----------
+        string : str
+            String to parse.
+
+        Returns
+        -------
+        powl.actiondata.TransactionData
+            Contains data to perform a transaction action.
+
+        Raises
+        ------
+        ValueError
+            If amount is not a float.
+            If a value for TransactionData is missing.
         """
         data = actiondata.AccountingData()
         params = re.split(self._TOKEN_FLAG, string)
-        
+
         for param in params:
             if re.match(self._ACCOUNTING_TOKEN_DEBIT, param):
                 data.debit = re.sub(self._ACCOUNTING_TOKEN_DEBIT, '', param)
@@ -72,7 +160,7 @@ class TransactionParser(object):
                 data.memo = re.sub(self._ACCOUNTING_TOKEN_MEMO, '', param)
                 data.memo = memo.replace("\"", '')
                 data.memo = memo.strip()
-        
+
         if not data.debit:
             raise ValueError("debit was not parsed")
         if not data.credit:
@@ -81,56 +169,11 @@ class TransactionParser(object):
             raise ValueError("amount was not parsed")
         if not data.memo:
             raise ValueError("memo was not parsed")
-                
+
         try:
             float(data.amount)
         except ValueError:
             raise ValueError("amount is not a number")
-        
+
         return data
 
-class BodyCompositionDataParser(object):
-    """
-    Parses a formatted string containing body composition data.
-    """
-    
-    _TOKEN_FLAG = '-'
-    _TOKEN_MASS = '^m'
-    _TOKEN_FAT_PERCENTAGE = '^f'
-
-    def parse(self, data):
-        """
-        Parse a formatted body composition string into a BodyCompositionData object.
-        
-        Args: 
-            string (str): String to parse.
-        
-        Raises:
-            ValueError: If mass or fat percentage is not a float. If a value for BodyCompositionData is missing.
-        """
-        data = actiondata.BodyCompositionData()
-        params = re.split(self._TOKEN_FLAG, data)
-        
-        for param in params:
-            if re.match(self._TOKEN_MASS, param):
-                data.mass = re.sub(self._TOKEN_MASS, '', param)
-                data.mass = data.mass.strip()
-            elif re.match(self._TOKEN_FAT, param):
-                data.fat_percentage = re.sub(self._TOKEN_FAT, '', param)
-                data.fat_percentage = data.fat_percentage.strip()
-                
-        if not data.mass:
-            raise ValueError("Mass was not parsed")
-        if not data.fat:
-            raise ValueError("Fat was not parsed")
-                
-        try:
-            float(data.mass)
-        except ValueError:
-            raise ValueError("Mass is not a number")
-        try:
-            float(data.fat_percentage)
-        except ValueError:
-            raise ValueError("Fat percentage is not a number")
-            
-        return data
