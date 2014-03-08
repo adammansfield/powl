586b038f54483df11b0a89502a4923aa90cd9f30
Adam Mansfield <adam@adammansfield.org>
2014-03-08 04:24:22 -0500

    Added tests for QIF date conversion.

--- test/test_action.py
+++ test/test_action.py
@@ -92,25 +92,34 @@ class TestActionTransaction(unittest.TestCase):
             
     # QIF CONVERSION
     ## CONVERT AMOUNT
-    def test_convert_amount_invalid_account(self):
-        """Test for an account that does not exist."""
-        debit = "non-existant account"
-        amount = "10.00"
+    def test_convert_amount_invalid_debit_account(self):
+        """Test for an debit accounts that are invalid and do not exist."""
+        valid_amount = "10.00"
+
+        invalid_debit = "non-existant account"
         with self.assertRaises(KeyError) as context:
-            self._action._convert_amount(debit, amount)
+            self._action._convert_amount(invalid_debit, valid_amount)
         self.assertEqual(
             context.exception.message,
-            "account ({0}) does not exist".format(debit))
+            "account ({0}) does not exist".format(invalid_debit))
 
     def test_convert_amount_invalid_amount(self):
-        """Test for an amount that cannot be converted to float."""
-        debit = self._ASSETS_KEYS[0]
-        amount = "foo"
+        """Test for amounts that cannot be converted to float."""
+        valid_debit = self._ASSETS_KEYS[0]
+
+        invalid_amount = "foo"
         with self.assertRaises(ValueError) as context:
-            self._action._convert_amount(debit, amount)
+            self._action._convert_amount(valid_debit, invalid_amount)
         self.assertEqual(
             context.exception.message,
-            "amount ({0}) cannot be converted to float".format(amount))
+            "amount ({0}) cannot be converted to float".format(invalid_amount))
+
+        invalid_amount = "5.01a"
+        with self.assertRaises(ValueError) as context:
+            self._action._convert_amount(valid_debit, invalid_amount)
+        self.assertEqual(
+            context.exception.message,
+            "amount ({0}) cannot be converted to float".format(invalid_amount))
     
     def test_convert_amount_debit_is_expense(self):
         """Test for output amount when debit is an expense."""
@@ -147,17 +156,60 @@ class TestActionTransaction(unittest.TestCase):
         actual = self._action._convert_amount(debit, amount)
         self.assertEqual(actual, expected)
 
-        amount = "100.455"
+        amount = "100.4550"
         expected = "100.45"
         actual = self._action._convert_amount(debit, amount)
         self.assertEqual(actual, expected)
         
-        amount = "100.456"
+        amount = "100.4551"
         expected = "100.46"
         actual = self._action._convert_amount(debit, amount)
         self.assertEqual(actual, expected)
 
+    ## CONVERT DATE
+    def test_convert_date_type_error(self):
+        """Test for a dates that are invalid."""
+        date = "invalid date type"
+        with self.assertRaises(TypeError) as context:
+            self._action._convert_date(date)
+        self.assertEqual(
+            context.exception.message,
+            "date ({0}) cannot be converted to MM/DD/YYYY ".format(date) +
+            "because argument must be 9-item sequence, not str")
 
+    def test_convert_date_overflow_error(self):
+        """Test for a date that cannot be converted to C long."""
+        date = (9999999999,0,0,0,0,0,0,0,0)
+        with self.assertRaises(OverflowError) as context:
+            self._action._convert_date(date)
+        self.assertEqual(
+            context.exception.message,
+            "date ({0}) cannot be converted to MM/DD/YYYY ".format(date) +
+            "because Python int too large to convert to C long")
+
+    def test_convert_date_value_error(self):
+        """Test for a date that is out of range."""
+        date = (1000,0,0,0,0,0,0,0,0)
+        with self.assertRaises(ValueError) as context:
+            self._action._convert_date(date)
+        self.assertEqual(
+            context.exception.message,
+            "date ({0}) cannot be converted to MM/DD/YYYY ".format(date) +
+            "because year out of range")
+
+    def test_convert_date_current_date(self):
+        """Test the expected output using the current date."""
+        date = localtime()
+        expected = time.strftime("%m/%d/%Y", date)
+        actual = self._action.convert_date(date)
+        self.assertEqual(actual, expected)
+
+    def test_convert_date_past_date(self):
+        """Test the expected output using a past date."""
+        date = time.gmtime(0)
+        expected = "01/01/1970"
+        actual = self._action.convert_date(date)
+        self.assertEqual(actual, expected)
 
 
     # VALIDITY
