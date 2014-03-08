d814336e409cb88819a0c141b77139435b0b3e54
Adam Mansfield <adam@adammansfield.org>
2014-03-08 18:10:22 -0500

    Added actiontype. Added action map.

---
+++ powl/actiontype.py
@@ -0,0 +1,20 @@
+"""Defines action types and an action map for shorthand keys."""
+
+# Action types.
+BODY_COMPOSITION = "bodycomposition"
+NOTE = "note"
+TRANSACTION = "transaction"
+
+# Maps keys to actions. Multiple keys can map to one action.
+ACTION_MAP = {
+    "b": BODY_COMPOSITION,
+    "body": BODY_COMPOSITION,
+    "bodycomposition": BODY_COMPOSITION,
+
+    "n": NOTE,
+    "note": NOTE,
+
+    "t": TRANSACTION,
+    "transaction": TRANSACTION
+}
+
