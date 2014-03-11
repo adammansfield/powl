3fbf90f6e416bb0e751d836cf1b91a3b3317d8c2
Adam Mansfield <adam@adammansfield.org>
2014-03-11 03:04:14 -0400

    Moved tranascation coneversion tests from test_action to test_transactionconverter.

--- test/test_transactionconverter.py
+++ test/test_transactionconverter.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 """Tests for TransactionAction in powl.action."""
+import time
 import unittest
 from tests.mocks.filesystem import MockFile
 from tests.mocks.parser import MockParser
-from powl.action import TransactionAction
+from powl.transactionconverter import TransactionConverter
 from powl.parser import TransactionParser
 
-class TestActionTransaction(unittest.TestCase):
+class TestTransactionConverter(unittest.TestCase):
     """Class for testing the transaction action."""
 
     _FILES = {
@@ -54,25 +55,19 @@ class TestActionTransaction(unittest.TestCase):
     }
     _EXPENSES_KEYS = _EXPENSES.keys()
 
-    # INITIALIZATION
     def setUp(self):
-        # TODO: set accounts in transactionprocessor before testing
         self._log = NullLogWriter()
-        self._parser = MockParser()
 
-        self._action = Transaction(
+        self._converter = QifConverter(
             self._log,
-            self._parser,
             self._FILES,
             self._ACCOUNT_TYPES,
             self._ASSETS,
             self._LIABILITIES,
             self._REVENUES,
-            self._EXPENSES
-        )
-    
-    # ACTION
-    def test_do_valid(self):
+            self._EXPENSES)
+
+    def test_convert(self):
         # TODO: put proper values here.
         string = "" # input
         date = ""   # time.struct_time
@@ -81,24 +76,30 @@ class TestActionTransaction(unittest.TestCase):
             "", # debit
             "", # credit
             "", # amount
-            "") # memo 
+            "") # memo
 
         try:
-            self._action.do(string, date)
+            self._converter.do(string, date)
         except:
             self.fail("do raised an unexpected exception")
 
         # TODO: assert mockfile._append_line_data == expected
-            
-    # QIF CONVERSION
-    ## CONVERT AMOUNT
+
+    def test_invalid_key_to_file(self):
+        # TODO: check to make sure that:
+        #         1. each key in filenames exists as a key in accounts
+        #         2. each key in account_types exists as a key in accounts
+        #       else throw a KeyValue Error and write a test for this
+        pass
+
+    # Convert amount tests.
     def test_convert_amount_invalid_debit_account(self):
         """Test for an debit accounts that are invalid and do not exist."""
         valid_amount = "10.00"
 
         invalid_debit = "non-existant account"
         with self.assertRaises(KeyError) as context:
-            self._action._convert_amount(invalid_debit, valid_amount)
+            self._converter._convert_amount(invalid_debit, valid_amount)
         self.assertEqual(
             context.exception.message,
             "account ({0}) does not exist".format(invalid_debit))
@@ -109,32 +110,32 @@ class TestActionTransaction(unittest.TestCase):
 
         invalid_amount = "foo"
         with self.assertRaises(ValueError) as context:
-            self._action._convert_amount(valid_debit, invalid_amount)
+            self._converter._convert_amount(valid_debit, invalid_amount)
         self.assertEqual(
             context.exception.message,
             "amount ({0}) cannot be converted to float".format(invalid_amount))
 
         invalid_amount = "5.01a"
         with self.assertRaises(ValueError) as context:
-            self._action._convert_amount(valid_debit, invalid_amount)
+            self._converter._convert_amount(valid_debit, invalid_amount)
         self.assertEqual(
             context.exception.message,
             "amount ({0}) cannot be converted to float".format(invalid_amount))
-    
+
     def test_convert_amount_debit_is_expense(self):
         """Test for output amount when debit is an expense."""
-        debit = self._EXPENSES_KEYS[0] 
+        debit = self._EXPENSES_KEYS[0]
         amount = "25.15"
         expected = "-25.15"
-        actual = self._action._convert_amount(debit, amount)
+        actual = self._converter._convert_amount(debit, amount)
         self.assertEqual(actual, expected)
 
     def test_convert_amount_debit_is_not_expense(self):
         """Test for output amount when debit is not an expense."""
-        debit = self._ASSETS_KEYS[0] 
+        debit = self._ASSETS_KEYS[0]
         amount = "10.75"
         expected = "10.75"
-        actual = self._action._convert_amount(debit, amount)
+        actual = self._converter._convert_amount(debit, amount)
         self.assertEqual(actual, expected)
 
     def test_convert_amount_output_is_two_decimal_places(self):
@@ -143,35 +144,35 @@ class TestActionTransaction(unittest.TestCase):
 
         amount = "5"
         expected = "5.00"
-        actual = self._action._convert_amount(debit, amount)
+        actual = self._converter._convert_amount(debit, amount)
         self.assertEqual(actual, expected)
 
         amount = "10.2"
         expected = "10.20"
-        actual = self._action._convert_amount(debit, amount)
+        actual = self._converter._convert_amount(debit, amount)
         self.assertEqual(actual, expected)
 
         amount = "20.45"
         expected = "20.45"
-        actual = self._action._convert_amount(debit, amount)
+        actual = self._converter._convert_amount(debit, amount)
         self.assertEqual(actual, expected)
 
         amount = "100.4550"
         expected = "100.45"
-        actual = self._action._convert_amount(debit, amount)
-        self.assertEqual(actual, expected)
-        
-        amount = "100.4551"
-        expected = "100.46"
-        actual = self._action._convert_amount(debit, amount)
+        actual = self._converter._convert_amount(debit, amount)
         self.assertEqual(actual, expected)
 
-    ## CONVERT DATE
+        amount = "100.4551"
+        expected = "100.46"
+        actual = self._converter._convert_amount(debit, amount)
+        self.assertEqual(actual, expected)
+
+    # Convert date tests.
     def test_convert_date_type_error(self):
         """Test for a dates that are invalid."""
         date = "invalid date type"
         with self.assertRaises(TypeError) as context:
-            self._action._convert_date(date)
+            self._converter._convert_date(date)
         self.assertEqual(
             context.exception.message,
             "date ({0}) cannot be converted to MM/DD/YYYY ".format(date) +
@@ -181,7 +182,7 @@ class TestActionTransaction(unittest.TestCase):
         """Test for a date that cannot be converted to C long."""
         date = (9999999999,0,0,0,0,0,0,0,0)
         with self.assertRaises(OverflowError) as context:
-            self._action._convert_date(date)
+            self._converter._convert_date(date)
         self.assertEqual(
             context.exception.message,
             "date ({0}) cannot be converted to MM/DD/YYYY ".format(date) +
@@ -191,7 +192,7 @@ class TestActionTransaction(unittest.TestCase):
         """Test for a date that is out of range."""
         date = (1000,0,0,0,0,0,0,0,0)
         with self.assertRaises(ValueError) as context:
-            self._action._convert_date(date)
+            self._converter._convert_date(date)
         self.assertEqual(
             context.exception.message,
             "date ({0}) cannot be converted to MM/DD/YYYY ".format(date) +
@@ -201,44 +202,25 @@ class TestActionTransaction(unittest.TestCase):
         """Test the expected output using the current date."""
         date = localtime()
         expected = time.strftime("%m/%d/%Y", date)
-        actual = self._action.convert_date(date)
+        actual = self._converter._convert_date(date)
         self.assertEqual(actual, expected)
 
     def test_convert_date_past_date(self):
         """Test the expected output using a past date."""
         date = time.gmtime(0)
         expected = "01/01/1970"
-        actual = self._action.convert_date(date)
+        actual = self._converter._convert_date(date)
         self.assertEqual(actual, expected)
 
+    # TODO: for both get_file and get_transfer:
+    #           - invalid debit
+    #           - invalid credit
+    #           - invalid debit and invalid credit
+    #           - debit with matching file and credit without matching file
+    #           - debit without matching file and credit with matching file
+    #           - debit without matching file and credit without matching file
 
-    # VALIDITY
-    ## ACCOUNTS
-    def test_validity_account_exists(self):
-        """Test for existing account."""
-        try:
-            self.transaction.validate_account("c")
-            self.transaction.validate_account("mm")
-            self.transaction.validate_account("int")
-            self.transaction.validate_account("mis")
-        except:
-            self.fail("validate_account raised an unexpected exception")
+    ## GET QIF FILE
 
-    def test_validity_account_not_exist(self):
-        """Test a not existing account."""
-        account = "foobar"
-        with self.assertRaises(ValueError) as context:
-            self.transaction.validate_account(account)
-        self.assertEqual(
-            context.exception.message, "account does not exist")
-
-    ## AMOUNT
-    def test_validity_amount_valid(self):
-        """Test a valid amount."""
-        try:
-            self.transaction.validate_amount(25)
-            self.transaction.validate_amount(3.01)
-            self.transaction.validate_amount("152.26")
-        except:
-            self.fail("validate_amount raised unexpected exception)
+    ## GET TRANSFER ACCOUNT
 
