408df326e63bbfda1c40e43a2c78253f1b7b399f
Adam Mansfield <adam@adammansfield.org>
2014-09-18 22:51:41 -0400

    Added run main.test.py and parser.test.py to runtests.sh.

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 echo
+echo "main.py"
+echo "======="
+python test/main.test.py
+
+echo
+echo "parser.py"
+echo "========="
+python test/parser.test.py
+
+echo
 echo "transactionconverter.py"
 echo "======================="
 python test/transactionconverter.test.py
+
