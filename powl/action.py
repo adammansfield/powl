3957c9f0240b910cc34167fe2de391e46dfa600b
Adam Mansfield <adam@adammansfield.org>
2014-09-18 23:04:30 -0400

    Moved ActionManager to action.py and removed actionmanager.py.

--- powl/action.py
+++ powl/action.py
@@ -1,5 +1,53 @@
 """Provides classes to perform specific actions."""
 import time
+from powl import actiontype
+
+class ActionManager:
+    """
+    Managers and provides methods for doing actions.
+    """
+
+    def __init__(self, log)
+        """
+        Parameters
+        ----------
+        log : powl.log.Log
+            Used to log.
+        """
+        self._log = log
+        self._accounting_action = accounting_action
+        self._body_composition_action = body_composition_action
+        self._note_action = note_action
+
+        self._action_type_to_action_map = {
+            actiontype.ACCOUNTING: self._accounting_action,
+            actiontype.BODYCOMPOSITION: self._bodycomposition_action,
+            actiontype.NOTE: self._note_action}
+
+    def do_action(self, action_type, action_data, action_date):
+        """
+        Do the specified action.
+
+        Args:
+            action_type: The type of action to do.
+            data: Action specific class containing all data required for the
+                specified action type.
+        """
+        try:
+            action = self._action_type_to_action_map[action_type]
+        except KeyError:
+            self._log.error(
+                "Action type '%s' is not in the action map",
+                action_type)
+        else:
+            try:
+                action.do(action_data, action_date)
+            except (IOError, OverflowError, TypeError, ValueError) as error:
+                self._log.error(
+                    "'%s' was raised by action '%s' with the message '%s'",
+                    type(error).__name__,
+                    action_type,
+                    error)
 
 class Action(object):
     """
