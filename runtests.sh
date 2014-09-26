d27037a82c5d9c2cceb60e76cb7bad00dc7732ed
Adam Mansfield <adam@adammansfield.org>
2014-09-26 01:16:26 -0400

    Added powl.exception to runtests.sh.

--- runtests.sh
+++ runtests.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
-echo "powl.action:"
+echo "powl.action"
+echo "-----------"
 python test/action.test.py
 
 echo "\n"
-echo "powl.parser:"
+echo "powl.exception"
+echo "--------------"
+python test/exception.test.py
+
+echo "\n"
+echo "powl.parser"
+echo "-----------"
 python test/parser.test.py
 
 echo "\n"
-echo "powl.transactionconverter:"
+echo "powl.transactionconverter"
+echo "-------------------------"
 python test/transactionconverter.test.py
 
