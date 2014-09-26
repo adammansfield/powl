014df99316b844ebbc8846848f7d9eef85ce9353
Adam Mansfield <adam@adammansfield.org>
2014-09-26 01:49:37 -0400

    Added positional parsers.

--- powl/parser.py
+++ powl/parser.py
@@ -2,6 +2,7 @@
 import re
 from powl import actiontype
 from powl import actiondata
+from powl import exception
 
 
 class Parser(object):
@@ -30,13 +31,6 @@ class ActionItemParser(Parser):
 
     def parse(self, item):
         """
-        Parse an action item into an action type and its data.
-
-        Parameters
-        ----------
-        item : str
-            Item to parse.
-
         Returns
         -------
         tuple of powl.actiontype and str
@@ -53,15 +47,17 @@ class ActionItemParser(Parser):
 
         try:
             action_type = actiontype.ACTION_MAP[action_key]
-        except KeyError:
-            raise KeyError("action key ({0}) is invalid".format(action_key))
+        except KeyError as err:
+            msg = "action key ({0}) is unknown".format(action_key)
+            exception.add_message(err, msg)
+            raise
         else:
             return action_type, data
 
 
 class BodyCompositionDataFlagParser(Parser):
     """
-    Parses a string containing body composition data.
+    Parses a string containing body composition data based on flags.
     """
 
     _TOKEN_FLAG = '-'
@@ -70,13 +66,6 @@ class BodyCompositionDataFlagParser(Parser):
 
     def parse(self, string):
         """
-        Parse a formatted string into a BodyCompositionData object.
-
-        Parameters
-        ----------
-        string : str
-            String to parse.
-
         Returns
         -------
         powl.actiondata.BodyCompositionData
@@ -89,7 +78,7 @@ class BodyCompositionDataFlagParser(Parser):
             If a value for BodyCompositionData is missing.
         """
         data = actiondata.BodyCompositionData()
-        params = re.split(self._TOKEN_FLAG, string)
+        params = string.split(self._TOKEN_FLAG)
 
         for param in params:
             if re.match(self._TOKEN_MASS, param):
@@ -100,26 +89,91 @@ class BodyCompositionDataFlagParser(Parser):
                 data.fat_percentage = data.fat_percentage.strip()
 
         if not data.mass:
-            raise ValueError("mass was not parsed from ({0})".format(string))
+            msg = "mass was not parsed from ({0})".format(string)
+            err = exception.create(ValueError, msg)
+            raise err
+
         if not data.fat_percentage:
-            raise ValueError("fat percentage was not parsed from "
-                             "({0})".format(string))
+            msg =("fat percentage was not parsed " +
+                  "from ({0})".format(string))
+            err = exception.create(ValueError, msg)
+            raise err
 
         try:
             float(data.mass)
-        except ValueError:
-            raise ValueError("mass is not a number ({0})".format(data.mass))
+        except ValueError as err:
+            msg = "mass ({0}) is not a number".format(data.mass)
+            exception.add_message(err, msg)
+            raise
+
         try:
             float(data.fat_percentage)
-        except ValueError:
-            raise ValueError("fat percentage is not a number "
-                             "({0})".format(data.fat_percentage))
+        except ValueError as err:
+            msg = ("fat percentage ({0}) ".format(data.fat_percentage) +
+                   "is not a number")
+            exception.add_message(err, msg)
+            raise
+
+        return data
+
+
+class BodyCompositionDataPositionalParser(Parser):
+    """
+    Parses a string containing body composition data based on position.
+    """
+
+    _DELIMITER = ' '
+    _POSITION_MASS = 0
+    _POSITION_FAT = 1
+    _NUM_PARAMS = 2
+    _MAX_SPLITS = _NUM_PARAMS - 1
+
+    def parse(self, string):
+        """
+        Returns
+        -------
+        powl.actiondata.BodyCompositionData
+            Contains data to perform a body composition action.
+
+        Raises
+        ------
+        ValueError
+            If mass or fat percentage is not a float.
+            If a value for BodyCompositionData is missing.
+        """
+        data = actiondata.BodyCompositionData()
+        params = re.split(self._DELIMITER, string, self._MAX_SPLITS)
+
+        if len(params) < self._NUM_PARAMS:
+            msg = "not enough arguments from ({0})".format(string)
+            err = exception.create(ValueError, msg)
+            raise err
+
+        mass = params[self._POSITION_MASS]
+        fat_percentage = params[self._POSITION_FAT]
+
+        try:
+            data.mass = float(mass)
+        except ValueError as err:
+            msg = "mass ({0}) is not a number".format(mass)
+            exception.add_message(err, msg)
+            raise
+
+        try:
+            data.fat_percentage = float(fat_percentage)
+        except ValueError as err:
+            msg =("fat percentage ({0}) ".format(fat_percentage) +
+                  "is not a number")
+            exception.add_message(err, msg)
+            raise
+
         return data
 
 
 class TransactionDataFlagParser(Parser):
     """
-    Parses a formatted string containing accounting data.
+    Parses a formatted string containing accounting data based on flag
+    arguments.
     """
 
     _TOKEN_FLAG = '-'
@@ -130,13 +184,6 @@ class TransactionDataFlagParser(Parser):
 
     def parse(self, string):
         """
-        Parse a formatted string into a TransactionData object.
-
-        Parameters
-        ----------
-        string : str
-            String to parse.
-
         Returns
         -------
         powl.actiondata.TransactionData
@@ -149,7 +196,7 @@ class TransactionDataFlagParser(Parser):
             If a value for TransactionData is missing.
         """
         data = actiondata.TransactionData()
-        params = re.split(self._TOKEN_FLAG, string)
+        params = string.split(self._TOKEN_FLAG)
 
         for param in params:
             if re.match(self._TOKEN_DEBIT, param):
@@ -167,18 +214,81 @@ class TransactionDataFlagParser(Parser):
                 data.memo = data.memo.strip()
 
         if not data.debit:
-            raise ValueError("debit was not parsed")
+            msg = "debit is missing from ({0})".format(string)
+            err = exception.create(ValueError, msg)
+            raise err
+
         if not data.credit:
-            raise ValueError("credit was not parsed")
+            msg = "credit is missing from ({0})".format(string)
+            err = exception.create(ValueError, msg)
+            raise err
+
         if not data.amount:
-            raise ValueError("amount was not parsed")
+            msg = "amount is missing from ({0})".format(string)
+            err = exception.create(ValueError, msg)
+            raise err
+
         if not data.memo:
-            raise ValueError("memo was not parsed")
+            msg = "memo is missing from ({0})".format(string)
+            err = exception.create(ValueError, msg)
+            raise err
+
+        try:
+            float(data.amount)
+        except ValueError as err:
+            message = "amount ({0}) is not a number".format(data.amount)
+            exception.add_message(err, message)
+            raise
+
+        return data
+
+
+class TransactionDataPositionalParser(Parser):
+    """
+    Parses a formatted string containing accounting data based on
+    positional arguments.
+    """
+
+    _DELIMITER = ' '
+    _POSITION_AMOUNT = 0
+    _POSITION_DEBIT = 1
+    _POSITION_CREDIT = 2
+    _POSITION_MEMO = 3
+    _NUM_PARAMS = 4
+    _MAX_SPLITS = _NUM_PARAMS - 1
+
+    def parse(self, string):
+        """
+        Returns
+        -------
+        powl.actiondata.TransactionData
+            Contains data to perform a transaction action.
+
+        Raises
+        ------
+        ValueError
+            If amount is not a float.
+            If a value for TransactionData is missing.
+        """
+        data = actiondata.TransactionData()
+        params = string.split(self._DELIMITER, self._MAX_SPLITS)
+
+        if len(params) < self._NUM_PARAMS:
+            msg = "not enough arguments from ({0})".format(string)
+            err = exception.create(ValueError, msg)
+            raise err
+
+        data.debit = params[self._POSITION_DEBIT]
+        data.credit = params[self._POSITION_CREDIT]
+        data.amount = params[self._POSITION_AMOUNT]
+        data.memo = params[self._POSITION_MEMO]
 
         try:
             float(data.amount)
         except ValueError:
-            raise ValueError("amount is not a number")
+            msg = "amount ({0}) is not a number".format(data.amount)
+            err = exception.create(ValueError, msg)
+            raise err
 
         return data
 
--- test/parser.test.py
+++ test/parser.test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import unittest
 from powl import actiontype
+from powl import exception
 from powl import parser
 
 class ActionItemParserTest(unittest.TestCase):
@@ -8,7 +9,18 @@ class ActionItemParserTest(unittest.TestCase):
     def setUp(self):
         self.parser = parser.ActionItemParser()
 
-    def test__parse__bodycomposition(self):
+    def test__parse__bodycomposition__key_is_b(self):
+        """
+        Test with string for a body composition action.
+        """
+        string = "b -m 136 -f 13.5"
+        expected_action_key = actiontype.BODY_COMPOSITION
+        expected_data = "-m 136 -f 13.5"
+        actual_action_key, actual_data = self.parser.parse(string)
+        self.assertEqual(expected_action_key, actual_action_key)
+        self.assertEqual(expected_data, actual_data)
+
+    def test__parse__bodycomposition__key_is_bodycomposition(self):
         """
         Test with string for a body composition action.
         """
@@ -19,7 +31,18 @@ class ActionItemParserTest(unittest.TestCase):
         self.assertEqual(expected_action_key, actual_action_key)
         self.assertEqual(expected_data, actual_data)
 
-    def test__parse__note(self):
+    def test__parse__note__key_is_n(self):
+        """
+        Test with string for a note action.
+        """
+        string = "n buy some coffee"
+        expected_action_key = actiontype.NOTE
+        expected_data = "buy some coffee"
+        actual_action_key, actual_data = self.parser.parse(string)
+        self.assertEqual(expected_action_key, actual_action_key)
+        self.assertEqual(expected_data, actual_data)
+
+    def test__parse__note__key_is_note(self):
         """
         Test with string for a note action.
         """
@@ -30,7 +53,18 @@ class ActionItemParserTest(unittest.TestCase):
         self.assertEqual(expected_action_key, actual_action_key)
         self.assertEqual(expected_data, actual_data)
 
-    def test__parse__transaction(self):
+    def test__parse__transaction__key_is_a(self):
+        """
+        Test with string for a transaction action.
+        """
+        string = "a -d out -c debitcard -a 10 -m dinner"
+        expected_action_key = actiontype.TRANSACTION
+        expected_data = "-d out -c debitcard -a 10 -m dinner"
+        actual_action_key, actual_data = self.parser.parse(string)
+        self.assertEqual(expected_action_key, actual_action_key)
+        self.assertEqual(expected_data, actual_data)
+
+    def test__parse__transaction__key_is_transaction(self):
         """
         Test with string for a transaction action.
         """
@@ -46,10 +80,10 @@ class ActionItemParserTest(unittest.TestCase):
         Test with string for an unknown action.
         """
         string = "write this message"
-        expected_message = "action key (write) is invalid"
+        expected_message = "action key (write) is unknown"
         with self.assertRaises(KeyError) as context:
             self.parser.parse(string)
-        actual_message = context.exception.message
+        actual_message = exception.get_message(context.exception)
         self.assertEqual(expected_message, actual_message)
 
 
@@ -57,28 +91,39 @@ class BodyCompositionDataFlagParserTest(unittest.TestCase):
 
     def setUp(self):
         self.parser = parser.BodyCompositionDataFlagParser()
-        self.validate_called = False
 
-    def test__parse__success(self):
+    def test__parse__fat_percentage_is_not_a_number(self):
         """
-        Test for expected mass and fat percentage.
+        Test for string where mass is not a number.
         """
-        string = "-m 200.1 -f 15.2"
-        expected_mass = "200.1"
-        expected_fat_percentage = "15.2"
-        actual = self.parser.parse(string)
-        self.assertEqual(expected_mass, actual.mass)
-        self.assertEqual(expected_fat_percentage, actual.fat_percentage)
+        string = "-m 200 -f 1i5.2"
+        expected_message = "fat percentage (1i5.2) is not a number"
+        with self.assertRaises(ValueError) as context:
+            self.parser.parse(string)
+        actual_message = exception.get_message(context.exception)
+        self.assertEqual(expected_message, actual_message)
+
+    def test__parse__mass_is_not_a_number(self):
+        """
+        Test for string where mass is not a number.
+        """
+        string = "-m 200a -f 15.2"
+        expected_message = "mass (200a) is not a number"
+        with self.assertRaises(ValueError) as context:
+            self.parser.parse(string)
+        actual_message = exception.get_message(context.exception)
+        self.assertEqual(expected_message, actual_message)
 
     def test__parse__missing_fat_percentage(self):
         """
         Test for string missing fat percentage value.
         """
         string = "-m 100"
-        expected_message = "fat percentage was not parsed from ({0})".format(string)
+        expected_message = ("fat percentage was not parsed "
+                            "from ({0})".format(string))
         with self.assertRaises(ValueError) as context:
             self.parser.parse(string)
-        actual_message = context.exception.message
+        actual_message = exception.get_message(context.exception)
         self.assertEqual(expected_message, actual_message)
 
     def test__parse__missing_mass(self):
@@ -89,30 +134,80 @@ class BodyCompositionDataFlagParserTest(unittest.TestCase):
         expected_message = "mass was not parsed from ({0})".format(string)
         with self.assertRaises(ValueError) as context:
             self.parser.parse(string)
-        actual_message = context.exception.message
+        actual_message = exception.get_message(context.exception)
         self.assertEqual(expected_message, actual_message)
+
+    def test__parse__sanity(self):
+        """
+        Test for expected mass and fat percentage.
+        """
+        string = "-m 200.1 -f 15.2"
+        expected_mass = "200.1"
+        expected_fat_percentage = "15.2"
+        actual = self.parser.parse(string)
+        self.assertEqual(expected_mass, actual.mass)
+        self.assertEqual(expected_fat_percentage, actual.fat_percentage)
+
+
+class BodyCompositionDataPositionalParserTest(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = parser.BodyCompositionDataPositionalParser()
 
     def test__parse__fat_percentage_is_not_a_number(self):
         """
-        Test for string where mass is not a number.
+        Test for string where fat percentage is not a number.
         """
-        string = "-m 200 -f 1i5.2"
-        expected_message = "fat percentage is not a number (1i5.2)"
+        string = "200 1i5.2"
+        expected_message = "fat percentage (1i5.2) is not a number"
         with self.assertRaises(ValueError) as context:
             self.parser.parse(string)
-        actual_message = context.exception.message
+        actual_message = exception.get_message(context.exception)
         self.assertEqual(expected_message, actual_message)
 
     def test__parse__mass_is_not_a_number(self):
         """
         Test for string where mass is not a number.
         """
-        string = "-m 200a -f 15.2"
-        expected_message = "mass is not a number (200a)"
+        string = "200a 15.2"
+        expected_message = "mass (200a) is not a number"
         with self.assertRaises(ValueError) as context:
             self.parser.parse(string)
-        actual_message = context.exception.message
+        actual_message = exception.get_message(context.exception)
         self.assertEqual(expected_message, actual_message)
+
+    def test__parse__missing_all_values(self):
+        """
+        Test for string missing all values.
+        """
+        string = ""
+        expected_message = "not enough arguments from ({0})".format(string)
+        with self.assertRaises(ValueError) as context:
+            self.parser.parse(string)
+        actual_message = exception.get_message(context.exception)
+        self.assertEqual(expected_message, actual_message)
+
+    def test__parse__missing_one_value(self):
+        """
+        Test for string missing one value.
+        """
+        string = "150"
+        expected_message = "not enough arguments from ({0})".format(string)
+        with self.assertRaises(ValueError) as context:
+            self.parser.parse(string)
+        actual_message = exception.get_message(context.exception)
+        self.assertEqual(expected_message, actual_message)
+
+    def test__parse__sanity(self):
+        """
+        Test for expected mass and fat percentage.
+        """
+        string = "200.1 15.2"
+        expected_mass = 200.1
+        expected_fat_percentage = 15.2
+        actual = self.parser.parse(string)
+        self.assertEqual(expected_mass, actual.mass)
+        self.assertEqual(expected_fat_percentage, actual.fat_percentage)
 
 
 class TransactionDataFlagParser(unittest.TestCase):
@@ -120,7 +215,82 @@ class TransactionDataFlagParser(unittest.TestCase):
     def setUp(self):
         self.parser = parser.TransactionDataFlagParser()
 
-    def test__parse__success(self):
+    def test__parse__memo_multiple_words_with_quotes(self):
+        """Test transaction parser for expected memo of multiple words."""
+        string = "-d con -c ps -a 250 -m \"bought contacts\""
+        expected = "bought contacts"
+        data = self.parser.parse(string)
+        self.assertEqual(expected, data.memo)
+
+        debit = "con"
+        credit = "pc"
+        amount = "250"
+        memo = "purchased contacts"
+        string = "-d {0} -c {1} -a {2} -m \"{3}\"".format(debit, credit,
+                                                          amount, memo)
+        actual = self.parser.parse(string)
+        self.assertEqual(debit, actual.debit)
+        self.assertEqual(credit, actual.credit)
+        self.assertEqual(amount, actual.amount)
+        self.assertEqual(memo, actual.memo)
+
+    def test__parse__amount_is_not_a_number(self):
+        """
+        Test that the method throws when amount is not a number.
+        """
+        amount = "35b"
+        string = "-d phone -c bank -a {0} -m bill".format(amount)
+        expected_message = "amount ({0}) is not a number".format(amount)
+        with self.assertRaises(ValueError) as context:
+            self.parser.parse(string)
+        actual_message = exception.get_message(context.exception)
+        self.assertEqual(expected_message, actual_message)
+
+    def test__parse__missing_amount(self):
+        """
+        Test that the method throws if string is missing amount flag.
+        """
+        string = "-d phone -c bank -m bill"
+        expected_message = "amount is missing from ({0})".format(string)
+        with self.assertRaises(ValueError) as context:
+            self.parser.parse(string)
+        actual_message = exception.get_message(context.exception)
+        self.assertEqual(expected_message, actual_message)
+
+    def test__parse__missing_debit(self):
+        """
+        Test that the method throws if string is missing debit flag.
+        """
+        string = "-c bank -a 5 -m bill"
+        expected_message = "debit is missing from ({0})".format(string)
+        with self.assertRaises(ValueError) as context:
+            self.parser.parse(string)
+        actual_message = exception.get_message(context.exception)
+        self.assertEqual(expected_message, actual_message)
+
+    def test__parse__missing_credit(self):
+        """
+        Test that the method throws if string is missing credit flag.
+        """
+        string = "-d phone -a 5 -m bill"
+        expected_message = "credit is missing from ({0})".format(string)
+        with self.assertRaises(ValueError) as context:
+            self.parser.parse(string)
+        actual_message = exception.get_message(context.exception)
+        self.assertEqual(expected_message, actual_message)
+
+    def test__parse__missing_memo(self):
+        """
+        Test that the method throws if string is missing memo flag.
+        """
+        string = "-d phone -c bank -a 5"
+        expected_message = "memo is missing from ({0})".format(string)
+        with self.assertRaises(ValueError) as context:
+            self.parser.parse(string)
+        actual_message = exception.get_message(context.exception)
+        self.assertEqual(expected_message, actual_message)
+
+    def test__parse__sanity(self):
         """
         Test with sample data for expected output.
         """
@@ -136,32 +306,91 @@ class TransactionDataFlagParser(unittest.TestCase):
         self.assertEqual(amount, actual.amount)
         self.assertEqual(memo, actual.memo)
 
-    def test_parse_transaction_memo_one_word_no_quote(self):
-        """Test transaction parser for expected memo of one word."""
-        string = "-d mai -c pc -a 1 -m timbit"
-        expected = "timbit"
-        data = self.parser.parse(string)
-        self.assertEqual(expected, data.memo)
-
-    def test_parse_transaction_memo_one_word_with_quote(self):
-        """Test transaction parser for expected memo of one word."""
-        string = "-d mai -c pc -a 1 -m \"timbit\""
-        expected = "timbit"
-        data = self.parser.parse(string)
-        self.assertEqual(expected, data.memo)
-
-    def test_parse_transaction_memo_multiple_words(self):
-        """Test transaction parser for expected memo of multiple words."""
-        string = "-d con -c ps -a 250 -m \"bought contacts\""
-        expected = "bought contacts"
-        data = self.parser.parse(string)
-        self.assertEqual(expected, data.memo)
-
 
 class TransactionDataPositionalParser(unittest.TestCase):
 
     def setUp(self):
         self.parser = parser.TransactionDataPositionalParser()
+
+    def test__parse__amount_is_not_a_number(self):
+        """
+        Test that the method throws when amount is not a number.
+        """
+        amount = "35b"
+        string = "{0} phone bank phone bill".format(amount)
+        expected_message = "amount ({0}) is not a number".format(amount)
+        with self.assertRaises(ValueError) as context:
+            self.parser.parse(string)
+        actual_message = exception.get_message(context.exception)
+        self.assertEqual(expected_message, actual_message)
+
+    def test__parse__memo_multiple_words(self):
+        """
+        Test transaction parser for expected memo of multiple words.
+        """
+        string = "250 con pc bought contacts"
+        expected = "bought contacts"
+        data = self.parser.parse(string)
+        self.assertEqual(expected, data.memo)
+
+    def test__parse__missing_all_values(self):
+        """
+        Test for string missing all values
+        """
+        string = ""
+        expected_message = "not enough arguments from ({0})".format(string)
+        with self.assertRaises(ValueError) as context:
+            self.parser.parse(string)
+        actual_message = exception.get_message(context.exception)
+        self.assertEqual(expected_message, actual_message)
+
+    def test__parse__missing_one_value(self):
+        """
+        Test for string missing one value.
+        """
+        string = "5.25 coffee debitcard"
+        expected_message = "not enough arguments from ({0})".format(string)
+        with self.assertRaises(ValueError) as context:
+            self.parser.parse(string)
+        actual_message = exception.get_message(context.exception)
+        self.assertEqual(expected_message, actual_message)
+
+    def test__parse__missing_two_values(self):
+        """
+        Test for string missing one value.
+        """
+        string = "5.25 coffee"
+        expected_message = "not enough arguments from ({0})".format(string)
+        with self.assertRaises(ValueError) as context:
+            self.parser.parse(string)
+        actual_message = exception.get_message(context.exception)
+        self.assertEqual(expected_message, actual_message)
+
+    def test__parse__missing_three_values(self):
+        """
+        Test for string missing one value.
+        """
+        string = "5.25"
+        expected_message = "not enough arguments from ({0})".format(string)
+        with self.assertRaises(ValueError) as context:
+            self.parser.parse(string)
+        actual_message = exception.get_message(context.exception)
+        self.assertEqual(expected_message, actual_message)
+
+    def test__parse__sanity(self):
+        """
+        Test with sample data for expected output.
+        """
+        debit = "mis"
+        credit = "ca"
+        amount = "5.25"
+        memo = "coffee"
+        string = "{0} {1} {2} {3}".format(amount, debit, credit, memo)
+        actual = self.parser.parse(string)
+        self.assertEqual(debit, actual.debit)
+        self.assertEqual(credit, actual.credit)
+        self.assertEqual(amount, actual.amount)
+        self.assertEqual(memo, actual.memo)
 
 
 if __name__ == '__main__':
